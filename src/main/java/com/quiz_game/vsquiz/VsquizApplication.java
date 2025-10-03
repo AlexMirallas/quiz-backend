26616dc2a204a39232355b08d838ff8327d2978c
AlexMirallas <command.shockwave1@gmail.com>
2025-10-03 11:00:03 +0200

    first commit

---
+++ src/main/java/com/quiz_game/vsquiz/VsquizApplication.java
@@ -0,0 +1,13 @@
+package com.quiz_game.vsquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quiz_game/vsquiz/VsquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz_game.vsquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
